50273bf5136f99008b17cf4a23d3b52b46a5882e
EclipseDaemon <rajvardhanspawar@gmail.com>
2026-07-01 19:59:01 +0530

    manural backpropagation done

--- core/value.py
+++ core/value.py
@@ -1,8 +1,11 @@
+import math
+
 class Value:
     def __init__(self,data,_children=(),_op='',label=''):
         self.data = data
         self._prev = set(_children)
         self.grad = 0.0
+        self._backward = lambda: None
         self._op = _op
         self.label = label
     
@@ -11,8 +14,44 @@ class Value:
     
     def __add__(self,other):
         out = Value(self.data + other.data,(self,other),'+')
+        def _backward():
+            self.grad = 1 * out.grad
+            other.grad = 1* out.grad 
+        out._backward = _backward
         return out
 
     def __mul__(self,other):
         out = Value(self.data * other.data,(self,other),'*')
+        def _backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad 
+        out._backward = _backward
         return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x)-1) / (math.exp(2*x)+1)
+        out = Value(t,(self,),'tanh')
+        def _backward():
+            self.grad = (1 - (t**2)) * out.grad
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        #topological sort
+        node_values = []
+        visted  = set()
+        def get_val(x):
+            if x not in visted:
+                visted.add(x)
+                for v in x._prev:
+                    get_val(v)
+                node_values.append(x)
+        get_val(self)
+
+        #add gradient for each node
+        self.grad = 1.00
+        for n in reversed(node_values):
+            n._backward()
+        
+
--- neuron.py
+++ neuron.py
@@ -1,4 +1,22 @@
 from core.value import Value
+from core.graph import draw_dot
 
-x = Value(2.00)
-print(x)
+# input of x1,x2
+x1 = Value(2.00,label='x1')
+x2 = Value(0.0,label='x2')
+
+#weight w1,w2
+w1 = Value(-3.0,label='w1')
+w2 = Value(1.0,label='w2')
+
+#bais of neuron
+b = Value(8,label='b')
+
+x1w1= x1*w1; x1w1.label='x1w1'
+x2w2= x2*w2; x2w2.label='x2w2'
+x1w1x2w2= x1w1 + x2w2; x1w1x2w2.label='x1*w1 + x2*w2'
+n = x1w1x2w2 + b; n.label = 'n'
+
+o = n.tanh()
+draw_dot(o)
+print(o)
