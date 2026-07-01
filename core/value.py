a22a30a0b57aff140bdfe65ca95d18b19854e56f
EclipseDaemon <rajvardhanspawar@gmail.com>
2026-07-01 23:39:20 +0530

    added more function to my Value Class

--- core/value.py
+++ core/value.py
@@ -12,7 +12,20 @@ class Value:
     def __repr__(self):
         return f"Value(data={self.data})"
     
+    def __rmul__(self,other):
+        return self * other
+    
+    def __truediv__(self,other):
+        return self * other**-1
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self,other):
+        return self + (-other)
+    
     def __add__(self,other):
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data + other.data,(self,other),'+')
         def _backward():
             self.grad += 1 * out.grad
@@ -21,6 +34,7 @@ class Value:
         return out
 
     def __mul__(self,other):
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data * other.data,(self,other),'*')
         def _backward():
             self.grad += other.data * out.grad
@@ -37,6 +51,26 @@ class Value:
         out._backward = _backward
         return out
     
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x),(self,),'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+    
+    def __pow__(self,other):
+        assert isinstance(other,(int,float))
+        out = Value(self.data**other,(self,),'power')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+
     def backward(self):
         #topological sort
         node_values = []
@@ -54,3 +88,8 @@ class Value:
         for n in reversed(node_values):
             n._backward()
 
+
+a = Value(2.0)
+b = Value(4.0)
+
+print(a/b)
